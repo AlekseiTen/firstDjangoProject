003cf8f8bf425ea75f8355a51c194be5322da956
Aleksei Ten <amailcn@mail.ru>
2024-08-28 21:08:05 +0300

    Сделал доп.задание *

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,6 @@ def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
-
+        message = request.POST.get('message')
         print(f'{name} {phone} {message}')
     return render(request, 'contacts.html')
