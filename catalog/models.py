7d0a528f98e798909daad4f1a1df68b333747124
Aleksei Ten <amailcn@mail.ru>
2024-09-05 19:44:35 +0300

    сделал fk

--- catalog/models.py
+++ catalog/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Наименование продукта")
-    description = models.TextField(verbose_name="Описание продукта", blank=True, null=True)
-    photo = models.ImageField(upload_to="product/photo", blank=True, null=True, verbose_name="Изображение продукта")
-    created_at = models.DateTimeField(blank=True, null=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(blank=True, null=True, verbose_name="Дата последнего изменения")
-
-    def __str__(self):
-        # Строковое отображение объекта
-        return f'{self.name} {self.description}'
-
-    class Meta:
-        verbose_name = 'Продукт'  # Настройка для наименования одного объекта
-        verbose_name_plural = 'Продукты'  # Настройка для наименования набора объектов
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name="Наименование категории")
     description = models.TextField(verbose_name="Описание категории", blank=True, null=True)
@@ -29,3 +11,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'  # Настройка для наименования одного объекта
         verbose_name_plural = 'Категории'  # Настройка для наименования набора объектов
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Наименование продукта")
+    description = models.TextField(verbose_name="Описание продукта", blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="сategories", blank=True, null=True)
+    photo = models.ImageField(upload_to="product/photo", blank=True, null=True, verbose_name="Изображение продукта")
+    created_at = models.DateTimeField(blank=True, null=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(blank=True, null=True, verbose_name="Дата последнего изменения")
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Продукты'  # Настройка для наименования набора объектов
