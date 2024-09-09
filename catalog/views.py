8f93d34a297756f51d024e8dbb22f55a5f40c74a
Aleksei Ten <amailcn@mail.ru>
2024-09-09 23:45:52 +0300

    оствавил контакт

--- catalog/views.py
+++ catalog/views.py
@@ -14,3 +14,12 @@ def products_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
     return render(request, 'products_detail.html', context)
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} {phone} {message}')
+    return render(request, 'contacts.html')
