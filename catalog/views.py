a5d4654d40dd9fcf1fb07330fe842a9063ac32a2
Aleksei Ten <amailcn@mail.ru>
2024-09-09 21:36:46 +0300

    добавил контекст

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
-def index(request):
-    return render(request, 'base.html')
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'base.html', context)
 
-def home(request):
-    return render(request, 'home.html')
-
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} {phone} {message}')
-    return render(request, 'contacts.html')
+# def home(request):
+#     return render(request, 'home.html')
+#
+# def contacts(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(f'{name} {phone} {message}')
+#     return render(request, 'contacts.html')
