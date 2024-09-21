f505449ac2926d085699b8f567c49bbb532cbd2b
Aleksei Ten <amailcn@mail.ru>
2024-09-21 18:04:50 +0300

    создал модель

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    body = models.TextField(verbose_name='содержимое')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост в блоге'
+        verbose_name_plural = 'Посты в блоге'
