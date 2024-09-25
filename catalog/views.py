e28b920dbf000aa0589f8068b51ab0bbe6f192f2
Aleksei Ten <amailcn@mail.ru>
2024-09-25 18:39:32 +0300

    добавил контакты

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
@@ -15,11 +16,15 @@ class ProductDetailView(DetailView):
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
 
+class ContactView(View):
+    template_name = 'catalog/contacts.html'
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} {phone} {message}')
-    return render(request, 'catalog/contacts.html')
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def contacts(request):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            print(f'{name} {phone} {message}')
