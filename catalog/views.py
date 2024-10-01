fa16c4664aa84b18aae67701d64f433abf1fb83c
Aleksei Ten <amailcn@mail.ru>
2024-10-01 23:06:14 +0300

    доработал формы

--- catalog/views.py
+++ catalog/views.py
@@ -35,31 +35,6 @@ class ProductUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy("catalog:product_update", kwargs={"pk": self.object.pk})
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        ProductFormset = inlineformset_factory(
-            Product, Version, form=VersionForm, extra=1
-        )
-
-        if self.request.method == "POST":
-            context_data["formset"] = ProductFormset(
-                self.request.POST, instance=self.object
-            )
-        else:
-            context_data["formset"] = ProductFormset(instance=self.object)
-
-        return context_data
-
-    def form_valid(self, form):
-        formset = self.get_context_data()["formset"]
-        self.object = form.save()
-
-        if formset.is_valid():
-            formset.instance = self.object
-            formset.save()
-
-        return super().form_valid(form)
-
 
 class ProductDetailView(DetailView):
     model = Product
