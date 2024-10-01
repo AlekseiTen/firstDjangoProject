117f54c9b398a47efdecbfa4029ce8ba8a7d0960
Aleksei Ten <amailcn@mail.ru>
2024-10-01 22:07:21 +0300

    создал класс миксин

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,8 +2,17 @@ from django.forms import ModelForm, forms, BooleanField
 
 from catalog.models import Product, Version
 
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name,  fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = 'form-check-input'
+            else:
+                fild.widget.attrs['class'] = 'form-control'
 
-class ProductForm(ModelForm):
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
