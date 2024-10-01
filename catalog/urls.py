e78fb5ad7999d3da582ffcb8034057d53b298cdf
Aleksei Ten <amailcn@mail.ru>
2024-10-01 21:27:31 +0300

    добавил путь к методу удалиния, создания и редактирования

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, contacts
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, contacts
 
 app_name = CatalogConfig.name
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path("product/create", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete", ProductDeleteView.as_view(), name="product_delete"),
     path('contacts/', contacts, name='contacts')
 ]
