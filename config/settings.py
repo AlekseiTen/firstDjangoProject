0212c66c99c305d2cb3cc48dbf25d0e197fcfe0e
Aleksei Ten <amailcn@mail.ru>
2024-10-10 00:20:55 +0300

    изменил настройки почты

--- config/settings.py
+++ config/settings.py
@@ -138,13 +138,13 @@ AUTH_USER_MODEL = "users.User"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-
-EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'Thugforlife20@yandex.ru'
-EMAIL_HOST_PASSWORD = 'gqfoycwcqbnproxh'
-EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'amailcn@mail.ru'
+EMAIL_HOST_PASSWORD = 'J8jAS9SjXnx8Wc7a0aEe'
 
-SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
