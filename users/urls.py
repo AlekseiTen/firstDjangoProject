43131ac825a35da1891f6f1a6e9cef1416f1e340
Aleksei Ten <amailcn@mail.ru>
2024-10-09 23:34:21 +0300

    отформатировал по pep8

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
-    path('email-confirm/<str:token>/', email_verification, name="email_confirm"),
+    path('email-confirm/<str:token>/', email_verification, name="email-confirm"),
     # path('password-reset/', password_reset_request, name='password_reset'),
 ]
