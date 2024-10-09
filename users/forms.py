04998f01711ca441dc63dad380a72560509fd423
Aleksei Ten <amailcn@mail.ru>
2024-10-09 22:19:08 +0300

    создал файл forms с формой регистрации, создал шаблон login, и прописал урл для loginview

---
+++ users/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import forms, EmailField
+
+from catalog.forms import StyleFormMixin
+from users.models import User
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
