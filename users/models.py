991d4b9c48f6f6e7e5d2c2a724a82b4c0cfac596
Aleksei Ten <amailcn@mail.ru>
2024-10-15 17:09:48 +0300

    поле country сделал не обязательным

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
     phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE, help_text="Введите номер телефона")
-    country = models.CharField(max_length=150, verbose_name="Страна", help_text="Введите страну проживания")
+    country = models.CharField(max_length=150, verbose_name="Страна", **NULLABLE, help_text="Введите страну проживания")
     avatar = models.ImageField(upload_to="users/avatars/", **NULLABLE, help_text="Загрузите свой аватар")
     token = models.CharField(max_length=100, verbose_name="Token", **NULLABLE)
 
